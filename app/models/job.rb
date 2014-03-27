9cb3d735b0029bd07578e9ebac6280b85a36c124
David Chun <daveedchun@gmail.com>
2014-03-27 10:16:42 -0700

    adjustment to query scopr for pg production

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,4 +1,9 @@
 class Job < ActiveRecord::Base
   has_many :images
-  scope :starts_with, -> (number) { where("number like ?", "#{number}%")}
+  
+  if Rails.env.production?
+    scope :starts_with, -> (number) { where("CAST(number AS TEXT) like ?", "#{number}%")}
+  else
+    scope :starts_with, -> (number) { where("number like ?", "#{number}%")}
+  end
 end
